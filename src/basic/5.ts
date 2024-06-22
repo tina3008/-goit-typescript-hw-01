7b782ca79e6d478ce6539d7ebe1c5d22997933b4
tina3008 <kicja_3@ukr.net>
2024-06-22 14:34:55 +0300

    1

---
+++ src/basic/5.ts
@@ -0,0 +1,13 @@
+enum DayOfWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+const isWeekend = (day: DayOfWeek) => {
+  return day === 6 || day === 5;
+};
---
+++ src/generics/1.ts
@@ -0,0 +1,11 @@
+
+import axios from "axios";
+
+async function fetchData<T>(url: string): T {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
